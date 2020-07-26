cef89462f0eb9b984e433df2dfdda5ddb87c3867
Walter <U201620367@upc.edu.pe>
2020-07-26 18:06:10 -0500

    agrega listado de usuarios

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,11 +1,16 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/app")
@@ -16,4 +21,34 @@ public class IndexController {
 		model.addAttribute("titulo", "hola Spring Framework! con Model");
 		return "index";
 	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Walter");
+		usuario.setApellido("Huacho");
+		usuario.setEmail("whuacho@email.com");
+
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		
+		return "perfil"; 
+	}
+
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de usuarios");
+
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios() {
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Walter", "Huacho", "whuacho@email.com"),
+				new Usuario("Daniel", "Huacho", "whuacho@email.com"),
+				new Usuario("Pedro", "Huacho", "whuacho@email.com"));
+
+		return usuarios;
+	}
+
 }
